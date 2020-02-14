2252cc86557a7351ed70fe065f1ac24610839d16
devilla <dev.koold@gmail.com>
2020-02-15 01:02:26 +0530

    import Identicon from 'identicon.js';

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import Identicon from 'identicon.js';
 import './App.css';
 import SocialNetwork from '../abis/SocialNetwork.json'
 import Navbar from './Navbar'
